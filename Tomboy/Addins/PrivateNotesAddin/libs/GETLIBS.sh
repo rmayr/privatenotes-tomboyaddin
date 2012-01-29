04054658d10689d8eee96c17b4ebfa32f50d4666
Paul Klingelhuber <mail.paul@gmx.net>
2012-01-29 21:18:24 +0100

    more advanced GETLIBS.sh shellscript

--- Tomboy/Addins/PrivateNotesAddin/libs/GETLIBS.sh
+++ Tomboy/Addins/PrivateNotesAddin/libs/GETLIBS.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
+## copies newly built libraries from the infinote-library. 
+## if the infinote library has not been built successfully for some reason
+## there is a fallback which downloads some precompiled versions
+##
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -f $DIR/../../../../../InfinoteLib/Infinote/Infinote/bin/Debug/*.dll ./
+LIBSDIR="$DIR/../../../../../InfinoteLib/Infinote/Infinote/bin/Debug"
+
+LIBSDIR="$( readlink -m $LIBSDIR )"
+
+echo "using "
+echo $LIBSDIR/Infinote.dll
+
+if ([ -d $LIBSDIR/ ] && [ -e $LIBSDIR/Infinote.dll ]); then
+	echo "file exists :)"
+else
+	mkdir -p $LIBSDIR
+	echo "Infinote.dll does not exist in InfinoteLib/Infinote/Infinote/bin/Debug, downloading precompiled version"
+	if [ -e $LIBSDIR/precompiled.zip ]; then
+		echo "file already here, not downloading again..."
+	else
+			wget -O $LIBSDIR/precompiled.zip http://dl.dropbox.com/u/1526874/PrivateNotes/precompiledlibs.zip
+		if [ "$?" -ne 0 ]     # test if we finished
+		then
+			echo "Error while downloading file... exiting"
+		    exit
+		fi
+	fi
+	unzip -o $LIBSDIR/precompiled.zip -d $LIBSDIR
+fi
+
+cp -f $LIBSDIR/*.dll ./
